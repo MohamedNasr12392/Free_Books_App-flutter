2299d6a0acfeec080160375d3db6ef5e4747fc71
MohamedNasr12392 <midodaana912@gmail.com>
2023-12-16 08:44:17 +0200

    add book image to book details page

--- lib/Features/home/presentation/views/widgets/book_details_body_view.dart
+++ lib/Features/home/presentation/views/widgets/book_details_body_view.dart
@@ -1,33 +1,25 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_details_appbar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/horizontal_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    var width = MediaQuery.of(context).size.width ;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          BookDetailsAppBar(),
+          const BookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.18),
+            child: const HorizontalListItem(),
+          ),
         ],
       ),
     );
   }
 }
 
-class BookDetailsAppBar extends StatelessWidget {
-  const BookDetailsAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(onPressed: (){}, icon:const Icon( Icons.close)) ,
-        const Spacer(),
-        IconButton(onPressed: (){}, icon:const Icon(Icons.shopping_cart_outlined)),
-      ],
-    );
-  }
-}
